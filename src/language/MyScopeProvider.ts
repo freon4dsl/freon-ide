a53d0d57c99f0f481a47f3abf6ef669aa85f2f8d
Jos Warmer <jos.warmer@openmodeling.nl>
2025-02-18 15:20:15 +0100

    Adding validator, scoper and expresions grammar Weird cyclic loop when referring to Limited Instance (commented out now)

--- src/language/MyScopeProvider.ts
+++ src/language/MyScopeProvider.ts
@@ -1,5 +1,6 @@
 import { ReferenceInfo, Scope, ScopeProvider, AstUtils, LangiumCoreServices, AstNodeDescriptionProvider, MapScope, EMPTY_SCOPE, DefaultScopeProvider, AstNode, Reference } from "langium";
-import { ClassifierType, Concept, Expression, Interface, isClassifier, isConcept, isFreonModel, isProjection, Limited, ModelUnit } from "./generated/ast.js";
+import { Classifier, ClassifierType, Concept, ExpressionConcept, Interface, isClassifier, isConcept, isConceptDefinition, isConceptRule, isExpressionConcept, isFreonModel, isInterface, isProjection, Limited, ModelUnit, Property } from "./generated/ast.js";
+import { visitAndMap } from "../utils'/graphs.js";
 
 export class MyScopeProvider implements ScopeProvider {
     private astNodeDescriptionProvider: AstNodeDescriptionProvider;
@@ -9,7 +10,7 @@ export class MyScopeProvider implements ScopeProvider {
     }
 
     getScope(context: ReferenceInfo): Scope {
-        console.log(`My Scoper for ${context.property} container ${context.container?.$type}`)
+        // console.log(`My Scoper for ${context.property} container ${context.container?.$type}`)
         //make sure which cross-reference you are handling right now
         if(isConcept(context.container) && context.property === 'base') {
             //Success! We are handling the cross-reference of a greeting to a person!
@@ -18,7 +19,7 @@ export class MyScopeProvider implements ScopeProvider {
             const model = AstUtils.getContainerOfType(context.container, isFreonModel)!;
             //select all persons from this document
             const concepts = model.classifiers.filter(c => isConcept(c)).map(cc => cc)
-            console.log("Concepts: " + concepts.map(i => i.kind + ":" + i.name))
+            // console.log("Concepts: " + concepts.map(i => i.kind + ":" + i.name))
             //transform them into node descriptions
             const descriptions = concepts.map(p => this.astNodeDescriptionProvider.createDescription(p, p.name));
             //create the scope
@@ -39,33 +40,75 @@ export class MyScopeProvider2 extends DefaultScopeProvider {
 
     override getScope(context: ReferenceInfo): Scope {
         // First see whether this is a "self.property" in the .edit file
+        console.log(`========== context for property '${context.property}' + '${context.reference.$refText}' + '${context.container.$type}'`)
         if (context.property === "propName") {
-            console.log(`========== context for propName is property '${context.property}' + '${context.reference.$refText}'`)
             const projection = this.containerOfType(context.container, "Projection")
-            console.log(`    Projection found is ${projection} type '${projection?.$type}`)
+            // console.log(`    Projection found is ${projection} type '${projection?.$type}`)
             if (isProjection(projection)) {
                 const classifierReference = getClassifierType(projection.classifier)
-                console.log(`    !!isProjection ${classifierReference?.$refText}  ${classifierReference?.ref} ${classifierReference?.error?.message} ${classifierReference?.error?.property} `)
-                // console.log(`CT ${classifierReference.$refText} ${classifierReference.ref} ${classifierReference.error} ${classifierReference.$nodeDescription}`)
+                // console.log(`    !!isProjection ${classifierReference?.$refText}  ${classifierReference?.ref} ${classifierReference?.error?.message} ${classifierReference?.error?.property} `)
                 const classifierRef = classifierReference?.ref
-                console.log("    ClassfierRef " + classifierRef?.$type)
+                // console.log("    ClassfierRef " + classifierRef?.$type)
                 if (isClassifier(classifierRef)) {
-                    if (classifierRef === undefined || classifierRef?.properties?.length === 0) {
-                        console.log("    Found concept " + classifierRef.name + " " + " NOPROPS  " + classifierRef.properties.map(p => p.name + " "))
-                    } else {
-                        console.log("    Found concept " + classifierRef.name + " " + " props " + classifierRef.properties.map(p => p.name + " "))
-                    }
-                    const descriptions = classifierRef.properties.map(p => this.astNodeDescriptionProvider.createDescription(p, p.name));
+                    const descriptions = allProperties(classifierRef).map(p => this.astNodeDescriptionProvider.createDescription(p, p.name));
                     return new MapScope(descriptions)
+                }
+            } else {
+                const scopeDef = this.containerOfType(context.container, "ConceptDefinition")
+                if (isConceptDefinition(scopeDef)) {
+                    const classifierReference = getClassifierType(scopeDef.cref)
+                    const classifierRef = classifierReference?.ref
+                    if (isClassifier(classifierRef)) {
+                        const descriptions = allProperties(classifierRef).map(p => this.astNodeDescriptionProvider.createDescription(p, p.name));
+                        return new MapScope(descriptions)
+                    }
+                } else {
+                    const validDef = this.containerOfType(context.container, "ConceptRule")
+                    if (isConceptRule(validDef)) {
+                        const classifierReference = getClassifierType(validDef.conceptRef)
+                        const classifierRef = classifierReference?.ref
+                        if (isClassifier(classifierRef)) {
+                            const descriptions = allProperties(classifierRef).map(p => this.astNodeDescriptionProvider.createDescription(p, p.name));
+                            return new MapScope(descriptions)
+                        }
+                    }
                 }
             }
                 
+        } else { 
+            // const instanceExpr = this.containerOfType(context.container, "InstanceExpression")
+            // if (isInstanceExpression(instanceExpr)) {
+            //     if (context.property="instance") {
+            //         console.log("==================== " + context.property + "  in " + instanceExpr.$type )
+            //         // TODO take supers of Limited into account
+            //             const node: InstanceExpression = instanceExpr as InstanceExpression
+            //             console.log("== node.conceptName " + node.conceptName.$refText)
+            //             console.log("== node.description " + node.conceptName.$nodeDescription)
+            //             const nodeRefOk = node.conceptName?.ref !== undefined
+            //             const REF = node.conceptName?.ref  
+            //             if (nodeRefOk && isLimited(REF)) {
+            //                 const limited: Limited = node.conceptName.ref as Limited
+            //                 // console.log("==================== " + (context.reference.ref as InstanceExpression).conceptName?.ref?.conceptName?.ref)
+            //                 if (limited.instances !== undefined) {
+            //                     const instances: Instance[] = limited.instances
+            //                     const descript: AstNodeDescription[] = instances.map(ins => this.astNodeDescriptionProvider.createDescription(ins, ins.name))
+            //                     return new MapScope(descript)
+            //                 }
+            //             }
+            //     }
+            // }
         }
         // return default
         const result = super.getScope(context)
         return result
     }
 
+    /**
+     * 
+     * @param node Find the nearest container of type `type`.
+     * @param type 
+     * @returns 
+     */
     containerOfType(node: AstNode, type: string): AstNode | undefined {
         // console.log(`containerOfType ${node.$type}`)
         let result: AstNode | undefined = node
@@ -80,21 +123,97 @@ export class MyScopeProvider2 extends DefaultScopeProvider {
     }
 }
 
-function getClassifierType(ct: ClassifierType): Reference<Concept | Expression | Interface | Limited | ModelUnit> | undefined {
+function allProperties(classifier: Classifier | undefined): Property[] {
+    if (classifier === undefined) {
+        return [];
+    }
+    const result: Property[] = []
+    result.push(...classifier.properties)
+    allSuperClassifiers(classifier).forEach(cref =>
+        result.push(...cref.properties)
+    )
+    console.log(`All proprties of ${classifier.name}: ${result.map(p => p.name)}`)
+    return result;
+}
+
+function getClassifierType(ct: ClassifierType): Reference<Concept | ExpressionConcept | Interface | Limited | ModelUnit> | undefined {
     if (ct.conceptType !== undefined) {
         return ct.conceptType
     }
-    // if (ct.expressionType !== undefined) {
-    //     return ct.expressionType
-    // }
+    if (ct.expressionType !== undefined) {
+        return ct.expressionType
+    }
     if (ct.intfaceType !== undefined) {
         return ct.intfaceType
     }
-    // if (ct.limitedType !== undefined) {
-    //     return ct.limitedType
-    // }
-    // if (ct.modelunitType !== undefined) {
-    //     return ct.modelunitType
-    // }
+    if (ct.limitedType !== undefined) {
+        return ct.limitedType
+    }
+    if (ct.modelunitType !== undefined) {
+        return ct.modelunitType
+    }
     return undefined
+}
+
+/**
+ * Returns all super concepts, annotations and implemented interfaces of `classifier` recursively.
+ * @param classifier
+ */
+export function allSuperClassifiers(classifier: Classifier): Classifier[] {
+    return visitAndMap(superClassifiers, superClassifiers)(classifier)
+}
+
+/**
+ * Returns all direct super concepts, annotations and implemented interfaces of `classifier`
+ * @param classifier
+ */
+export function superClassifiers(classifier: Classifier): Classifier[]{
+    if (classifier === undefined) {
+        return [];
+    }
+    const result: Classifier[] = []
+
+    if (isConcept(classifier)) {
+        if (classifier.base !== undefined) {
+            if (classifier.base.ref !== undefined) {
+                result.push(classifier.base.ref)
+            }
+        }
+        if (classifier.implements !== undefined) {
+            if (classifier.name === 'Entity') {
+                console.log("WWW Entity interfaces " + classifier.implements.intfaces)
+            }
+            for(const intface of classifier.implements.intfaces) {
+                if (intface !== undefined && intface.ref !== undefined) {
+                    result.push(intface.ref)
+                }
+            }
+        }
+    } else if (isInterface(classifier) ) {
+        if (classifier.extends?.intfaces !== undefined) {
+            for(const intface of classifier.extends?.intfaces) {
+                if (intface !== undefined && intface.ref !== undefined) {
+                    result.push(intface.ref)
+                }
+            }
+        }
+    } else if (isExpressionConcept(classifier) ) {
+        if (classifier.base !== undefined) {
+            if (classifier.base.ref !== undefined) {
+                result.push(classifier.base.ref)
+            }
+        }
+        if (classifier.implements !== undefined) {
+            for(const intface of classifier.implements.intfaces) {
+                if (intface !== undefined && intface.ref !== undefined) {
+                    result.push(intface.ref)
+                }
+            }
+        }
+    }
+// // } else {
+    // //     throw new Error(`concept type ${typeof classifier} not handled`)
+    // }
+    console.log(`all supers of ${classifier.name} : ${result.map(r => r.name)}`)
+    return result
 }
